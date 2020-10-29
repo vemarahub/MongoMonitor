a5f09bf94620b17762998a516508b266a48ad445
vemara <vemarahub@gmail.com>
2020-10-29 11:14:23 +0530

    changed url for redirection

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for
 import db_details
 
 
@@ -8,35 +8,46 @@ app=Flask(__name__)
 
 
 
+
 @app.route('/', methods=['GET', 'POST'])
 def server():
-    final_list=[]
-    tot_size=""
-    instance=""
+   
     
     if request.method == 'POST':
         # Then get the data from the form
-        url = request.form['mongourl']
+    
+      url = request.form['mongourl']
         
-        hostname = request.form['hostname']
-        username = request.form['username']
-        password = request.form['password']
-        port = request.form['port']
+      hostname = request.form['hostname']
+      username = request.form['username']
+      password = request.form['password']
+      port = request.form['port']
        
-        if(url==""):
-            url = "mongodb://"+username+":"+password+"@"+hostname+":"+port+"/admin?retryWrites=true&w=majority"
+      if(url==""):
+          url = "mongodb://"+username+":"+password+"@"+hostname+":"+port+"/admin?retryWrites=true&w=majority"
        
-        final_list,tot_size,instance,version,uptime,hosts,primary,conn,user_list=db_details.db_info(url)
-        
-        if(instance==""):
-            return render_template("error.html")
-        else:
-            return render_template("home.html",instance=instance,final_list=final_list,tot_size=tot_size,version=version,uptime=uptime,hosts=hosts,primary=primary,conn=conn,user_list=user_list)
       
+      return redirect(url_for('home', url=url))
 
     else:   
         return render_template('index.html')
 
+@app.route('/home')
+def home():
+    final_list=[]
+    tot_size=""
+    instance=""
+    
+    url = request.args['url']
+    
+    final_list,tot_size,instance,version,uptime,hosts,primary,conn,user_list=db_details.db_info(url)
+       
+    if(instance==""):
+        return render_template("error.html")
+    else:
+        return render_template("home.html",instance=instance,final_list=final_list,tot_size=tot_size,version=version,uptime=uptime,hosts=hosts,primary=primary,conn=conn,user_list=user_list)
+      
+     
 
 if __name__ =='__main__':
     app.run(debug=True) 
